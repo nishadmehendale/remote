cd85c7c24e2f06083f84a952cfa63fec54f85c92
MEHENDALE <nishad.mehendale@capgemini.com>
2018-08-02 17:31:04 +0530

    aads

--- Day 10 MVC/src/MoneyMoneyBankApp/com/moneymoney/app/model/dao/MMBankCollection.java
+++
@@ -1,7 +0,0 @@
-package com.moneymoney.app.model.dao;
-
-import com.moneymoney.framework.account.dao.BankAccountCollection;
-
-public class MMBankCollection extends BankAccountCollection {
-
-}
--- Day 9/src/com/datetime/api/DateDigit.java
+++ Day 9/src/com/datetime/api/DateDigit.java
@@ -1,6 +1,8 @@
 package com.datetime.api;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class DateDigit {
@@ -9,9 +11,8 @@ public class DateDigit {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the date as DDMMYYYY");
 		String str = sc.nextLine();
-		StringBuffer s = new StringBuffer(str);
-		LocalDate datenow = LocalDate.of((int)(s.charAt(4)*1000 + s.charAt(5)*100 + s.charAt(6) * 10 + s.charAt(7)),(int)(s.charAt(2)*10 + s.charAt(3)),(int)(s.charAt(0)*10 + s.charAt(1)));
-		System.out.println(datenow.getDayOfMonth()+"/"+datenow.getMonthValue()+"/"+datenow.getYear());
-		System.out.println(datenow.getDayOfMonth()+" "+datenow.getMonth()+" ,"+datenow.getYear());
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyy");
+		LocalDate date = LocalDate.parse(str,formatter);
+		System.out.println(date);
 	}
 }
